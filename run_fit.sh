3c0037cc486d193bb570a3d5990ffb6e03dc708a
“Haifa <haifa.rejeb.sfar@cern.ch>
2024-11-18 15:57:15 +0100

    running background estimation using nohup

---
+++ run_fit.sh
@@ -0,0 +1,6 @@
+nohup python ttbar.py cen2016 > output1.log 2>&1 &
+nohup python ttbar.py fwd2016 > output2.log 2>&1 &
+nohup python ttbar.py cen2017 > output3.log 2>&1 &
+nohup python ttbar.py fwd2017 > output4.log 2>&1 &
+nohup python ttbar.py cen2018 > output5.log 2>&1 &
+nohup python ttbar.py fwd2018 > output6.log 2>&1 &
