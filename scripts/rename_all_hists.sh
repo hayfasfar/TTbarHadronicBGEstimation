a57ca82cbc60564f56060805015513f33bb1a52a
hayfasfar <hayfasfar@users.noreply.github.com>
2024-12-09 12:49:44 +0100

    corrected systematics for combined run2 fit

--- scripts/rename_all_hists.sh
+++ scripts/rename_all_hists.sh
@@ -1,4 +1,3 @@
-python rename_hists.py --file "TTbarAllHad16_*.root" --from "16" --to "Comb"
-python rename_hists.py --file "TTbarAllHad16APV_*.root" --from "16APV" --to "Comb"
-python rename_hists.py --file "TTbarAllHad17_*.root" --from "17" --to "Comb"
-python rename_hists.py --file "TTbarAllHad18_*.root" --from "18" --to "Comb"
+python rename_hists.py --file "/eos/home-h/hrejebsf/2Dalphabet_files/files_loosetomedium_Sep24/TTbarAllHad16_*.root" --from "16" --to "Comb"
+python rename_hists.py --file "/eos/home-h/hrejebsf/2Dalphabet_files/files_loosetomedium_Sep24/TTbarAllHad17_*.root" --from "17" --to "Comb"
+python rename_hists.py --file "/eos/home-h/hrejebsf/2Dalphabet_files/files_loosetomedium_Sep24/TTbarAllHad18_*.root" --from "18" --to "Comb"
