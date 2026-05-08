a56e4c2c89e27e57864409cbae7334ab8f68baa0
Aritra Mandal <mandalaritra1@gmail.com>
2026-05-08 08:33:56 -0500

    Adapt bg estimation workflow for cmslpc el9

--- ttbar.py
+++ ttbar.py
@@ -15,9 +15,9 @@ parser = argparse.ArgumentParser(description="input to the 2DAlphabet")
 parser.add_argument('--cat', type=str, help="category")
 parser.add_argument('--input', type=str, required=True, help="root files input path.")
 parser.add_argument('--output', type=str, default='output', help="root files input path.")
-parser.add_argument('--senario', choices=['RSGluon', 'ZPrime_1','ZPrime_10','ZPrime_30','ZPrime_DM'], required=True, help='Specify the signal senario to process limits: RSGluon or ZPrime.')
+parser.add_argument('--senario', '--scenario', dest='senario', choices=['RSGluon', 'ZPrime_1','ZPrime_10','ZPrime_30','ZPrime_DM'], required=True, help='Specify the signal scenario to process limits: RSGluon or ZPrime.')
 parser.add_argument('--signal', help='Specify a single signal to process (e.g., RSGluon2000).')
-parser.add_argument('--senario_fit', choices=['RSGluon', 'ZPrime'], help='Specify the signal senario to process the fit: RSGluon or ZPrime.')
+parser.add_argument('--senario_fit', '--scenario-fit', dest='senario_fit', choices=['RSGluon', 'ZPrime'], help='Specify the signal scenario to process the fit: RSGluon or ZPrime.')
 parser.add_argument('--tf', type=str, help="TF in case of Ftest study")
 parser.add_argument('--study',choices=['ftest', 'limit', 'fit', 'all'], default = 'all', type=str, help="running ttbar for specific study.")
 args = parser.parse_args()
@@ -30,8 +30,8 @@ tf = args.tf
 study = args.study
 output_dir = args.output
 json_file='jsons/config/ttbar_'+str(cat)+'.json'
-print ('json_file is', json_file)
-print ('senario is ', senario)
+print('json_file is', json_file)
+print('senario is ', senario)
 
 
 
@@ -42,9 +42,45 @@ def load_signals_from_json(json_signals, senario):
     if senario in data:
         signals_with_prefix = ['signal' + signal for signal in data[senario]]
         return signals_with_prefix
-    else:
-        print("Error: Scenario", senario, " not found in the JSON file.")
-        return []
+    print("Error: Scenario", senario, " not found in the JSON file.")
+    return []
+
+
+def signal_name(signal):
+    """Return the workspace signal name, including the expected signal prefix."""
+    return signal if signal.startswith('signal') else 'signal' + signal
+
+
+def signal_tag(signal):
+    """Return a stable directory/card tag without duplicating the signal prefix."""
+    return signal_name(signal)
+
+
+ROOT_COLOR_NAMES = {
+    1: 'black',
+    2: 'red',
+    3: 'green',
+    4: 'blue',
+    5: 'yellow',
+    6: 'magenta',
+    7: 'cyan',
+    8: 'green',
+    9: 'blue',
+}
+
+
+def normalize_process_colors(config):
+    """Convert legacy ROOT numeric color codes to 2DAlphabet color names."""
+    for process in config.get('PROCESSES', {}).values():
+        color = process.get('COLOR')
+        if color is None:
+            continue
+        try:
+            color_code = int(color)
+        except (TypeError, ValueError):
+            continue
+        process['COLOR'] = ROOT_COLOR_NAMES.get(color_code, 'black')
+
 
 signals = load_signals_from_json('jsons/signals.json', senario)
 
@@ -53,12 +89,13 @@ with open(json_file, 'r') as file:
     data = json.load(file, object_pairs_hook=OrderedDict)
 
 
-if 'GLOBAL' in data :
-	data['GLOBAL']['path'] = path
-        data['GLOBAL']['SIGNAME']= signals
+if 'GLOBAL' in data:
+    data['GLOBAL']['path'] = path
+    data['GLOBAL']['SIGNAME'] = signals
+normalize_process_colors(data)
   
 with open(json_file, 'w') as file:
-        json.dump(data, file, indent=4)
+    json.dump(data, file, indent=4)
 
 def get_transfer_function(cat):
     with open("jsons/TransferFunctions.json", "r") as file:
@@ -90,9 +127,9 @@ else :
  
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-savedirname = output_dir+'/ttbarfits_'+cat+'_'+dname+params
+savedirname = output_dir + '/ttbarfits_' + cat + '_' + dname + params
 
-print 'saving to {0}'.format(savedirname)
+print('saving to {0}'.format(savedirname))
 
 def _generate_constraints(nparams):
     out = {}
@@ -133,7 +170,7 @@ _rpf_options = {
         'constraints': _generate_constraints(4)
     },
     '1x3': {
-        'form': '0.1*(@0+@1*x)*(1+23*y+@3*y*y+@4*y*y*y)',
+        'form': '0.1*(@0+@1*x)*(1+@2*y+@3*y*y+@4*y*y*y)',
         'constraints': _generate_constraints(5)
     },
     '2x1': {
@@ -162,23 +199,23 @@ _rpf_options = {
     },
     '4x4': {
         'form': '0.1*(@0+@1*x+@2*x*x+@3*x*x*x+ @7*x**4)*(1+@4*y+@5*y*y+@6*y*y*y+@8*y**4)',
-        'constraints': _generate_constraints(7)
+        'constraints': _generate_constraints(9)
     },
     '5x5': {
         'form': '0.1*(@0+@1*x+@2*x*x+@3*x*x*x+ @7*x**4+@9*exp(x)+@10*sqrt(x))*(1+@4*y+@5*y*y+@6*y*y*y+@8*y**4)',
-        'constraints': _generate_constraints(7)
+        'constraints': _generate_constraints(11)
     },
     'sqrt': {
         'form': '0.1*(@0+@1*sqrt(x))*(1+@2*y)',
-        'constraints': _generate_constraints(7)
+        'constraints': _generate_constraints(3)
     },
     'sqrtexp': {
         'form': '0.1*(@0+@1*sqrt(x)+@2*exp(x))*(1+@3*y)',
-        'constraints': _generate_constraints(7)
+        'constraints': _generate_constraints(4)
     },
     'sqrtexplog': {
         'form': '0.1*(@0+@1*sqrt(x)+@2*log(x)+@3*exp(x))*(1+@4*y)',
-        'constraints': _generate_constraints(7)
+        'constraints': _generate_constraints(5)
     }
 }
 
@@ -311,12 +348,13 @@ def ML_fit(signal):
 
     # the default workspace directory, created in make_workspace(), is called ttbarfits16_3x1/
     twoD = TwoDAlphabet(savedirname,json_file, loadPrevious=True)
+    signame = signal_name(signal)
 
     # Create a subset of the primary ledger using the select() method.
     # The select() method takes as a function as its first argument
     # and any args to pass to that function as the remiaining arguments
     # to select(). See _select_signal for how to construct the function.
-    subset = twoD.ledger.select(_select_signal, 'signal{}'.format(signal))
+    subset = twoD.ledger.select(_select_signal, signame)
 
     # Make card reads the ledger and creates a Combine card from it.
     # The second argument specifices the sub-directory to save the card in.
@@ -327,16 +365,16 @@ def ML_fit(signal):
     # workspace is desired. Additionally, a different dataset can be supplied via
     # toyData but this requires supplying almost the full Combine card line and
     # is reserved for quick hacks by those who are familiar with Combine cards.
-    twoD.MakeCard(subset, 'ttbar-{}_area'.format(signal))
+    twoD.MakeCard(subset, 'ttbar-{}_area'.format(signal_tag(signal)))
 
     # Run the fit! Will run in the area specified by the `subtag` (ie. sub-directory) argument
     # and use the card in that area. Via the cardOrW argument, a different card or workspace can be
     # supplied (passed to the -d option of Combine).
-    twoD.MLfit('ttbar-{}_area'.format(signal),rMin=rmin,rMax=rmax,verbosity=0,extra=extra)
+    twoD.MLfit('ttbar-{}_area'.format(signal_tag(signal)),rMin=rmin,rMax=rmax,verbosity=0,extra=extra)
     
-    print 'twoD.GetParamsOnMatch()'
-    fitparams = twoD.GetParamsOnMatch(regex='', subtag='ttbar-{}_area'.format(signal), b_or_s='b')
-    print 'ttbar_xsec', fitparams['ttbar_xsec']
+    print('twoD.GetParamsOnMatch()')
+    fitparams = twoD.GetParamsOnMatch(regex='', subtag='ttbar-{}_area'.format(signal_tag(signal)), b_or_s='b')
+    print('ttbar_xsec', fitparams['ttbar_xsec'])
     
     with open("fitparams.json", "w") as outfile: 
         json.dump(fitparams, outfile)
@@ -346,9 +384,10 @@ def plot_fit(signal):
     Plots the fits from ML_fit() using 2DAlphabet
     '''
     twoD = TwoDAlphabet(savedirname, json_file , loadPrevious=True)
-    subset = twoD.ledger.select(_select_signal, 'signal{}'.format(signal))
-    twoD.StdPlots('ttbar-{}_area'.format(signal), subset)
-#     twoD.StdPlots('ttbar-{}_area'.format(signal), subset, prefit=True)
+    signame = signal_name(signal)
+    subset = twoD.ledger.select(_select_signal, signame)
+    twoD.StdPlots('ttbar-{}_area'.format(signal_tag(signal)), subset)
+#     twoD.StdPlots('ttbar-{}_area'.format(signal_tag(signal)), subset, prefit=True)
 
 def perform_limit(signal):
     '''
@@ -361,29 +400,24 @@ def perform_limit(signal):
     twoD = TwoDAlphabet(savedirname, json_file , loadPrevious=True)
 
     # GetParamsOnMatch() opens up the workspace's fitDiagnosticsTest.root and selects the rratio for the background
-    params_to_set = twoD.GetParamsOnMatch('rratio*', 'ttbar-{}_area'.format(signal), 'b')
+    params_to_set = twoD.GetParamsOnMatch('rratio*', 'ttbar-{}_area'.format(signal_tag(signal)), 'b')
     params_to_set = {k:v['val'] for k,v in params_to_set.items()}
 
-    # The iterWorkspaceObjs attribute stores the key-value pairs in the JSON config
-    # where the value is a list. This allows for later access like here so the user
-    # can loop over the list values without worrying if the config has changed over time
-    # (necessitating remembering that it changed and having to hard-code the list here).
-    for signame in twoD.iterWorkspaceObjs['SIGNAME']:
-        # signame is going too look like 16_<what we want> so drop the first three characters
-        print ('Performing limit for %s'%signame)
+    signame = signal_name(signal)
+    print('Performing limit for %s' % signame)
 
-        # Make a subset and card as in ML_fit()
-        subset = twoD.ledger.select(_select_signal, signame)
-        twoD.MakeCard(subset, signame+'_area')
-        # Run the blinded limit with our dictionary of TF parameters
-        # NOTE: we are running without blinding (blinding seems to cause an issue with the limit plotting script...)
-        twoD.Limit(
-             subtag=signame+'_area',
-             blindData=False,
-             verbosity=0,
-             setParams=params_to_set,
-             condor=False
-         )
+    # Make a subset and card as in ML_fit()
+    subset = twoD.ledger.select(_select_signal, signame)
+    twoD.MakeCard(subset, signame + '_area')
+    # Run the blinded limit with our dictionary of TF parameters
+    # NOTE: we are running without blinding (blinding seems to cause an issue with the limit plotting script...)
+    twoD.Limit(
+        subtag=signame + '_area',
+        blindData=False,
+        verbosity=0,
+        setParams=params_to_set,
+        condor=False
+    )
         
         
 def GoF(signal, tf='', nToys=100, condor=False):
@@ -395,27 +429,28 @@ def GoF(signal, tf='', nToys=100, condor=False):
     fitDir = savedirname
     twoD = TwoDAlphabet(fitDir, '{}/runConfig.json'.format(fitDir), loadPrevious=True)
     # Creates a Combine card if not already existing (it should exist if you've already fitted this workspace)
-    if not os.path.exists(twoD.tag+'/'+'signal{}_area/card.txt'.format(signal)):
-        print('{}/signal{}_area/card.txt does not exist, making card'.format(twoD.tag,signal))
-        subset = twoD.ledger.select(_select_signal, 'signal{}'.format(signal), tf)
-        twoD.MakeCard(subset, 'signal{}_area'.format(signal))
+    signame = signal_name(signal)
+    if not os.path.exists(twoD.tag + '/' + '{}_area/card.txt'.format(signame)):
+        print('{}/{}_area/card.txt does not exist, making card'.format(twoD.tag, signame))
+        subset = twoD.ledger.select(_select_signal, signame, tf)
+        twoD.MakeCard(subset, '{}_area'.format(signame))
 
     # Now run Combine's Goodness of Fit method, either on Combine or locally. 
     if condor == False:
         twoD.GoodnessOfFit(
-            'signal{}_area'.format(signal), ntoys=nToys, freezeSignal=0,
+            '{}_area'.format(signame), ntoys=nToys, freezeSignal=0,
             condor=False
         )
-	# Once finished, we can plot the results immediately from the output rootfile.
-	plot_GoF(signal, tf, condor)
+        # Once finished, we can plot the results immediately from the output rootfile.
+        plot_GoF(signame, tf, condor)
     else:
-	# 500 (default) toys, split across 50 condor jobs
+        # 500 (default) toys, split across 50 condor jobs
         twoD.GoodnessOfFit(
-            'signal{}_area'.format(signal), ntoys=nToys, freezeSignal=0,
-            condor=False, njobs=50
+            '{}_area'.format(signame), ntoys=nToys, freezeSignal=0,
+            condor=True, njobs=50
         )
-	# If submitting GoF jobs on condor, you must first wait for them to finish before plotting. 
-	print('Jobs successfully submitted - you can run plot_GoF after the jobs have finished running to plot results')
+        # If submitting GoF jobs on condor, you must first wait for them to finish before plotting. 
+        print('Jobs successfully submitted - you can run plot_GoF after the jobs have finished running to plot results')
     
 def doSignalInjection(signal, tf='', injectedAmount=2000.000, nToys=500, condor=False):
     '''
@@ -426,26 +461,27 @@ def doSignalInjection(signal, tf='', injectedAmount=2000.000, nToys=500, condor=
     fitDir = savedirname
     twoD = TwoDAlphabet(fitDir, '{}/runConfig.json'.format(fitDir), loadPrevious=True)
     # Creates a Combine card if not already existing (it should exist if you've already fitted this workspace)
-    if not os.path.exists(twoD.tag+'/'+'signal{}_area/card.txt'.format(signal)):
-        print('{}/signal{}_area/card.txt does not exist, making card'.format(twoD.tag,signal))
-        subset = twoD.ledger.select(_select_signal, 'signal{}'.format(signal), tf)
-        twoD.MakeCard(subset, 'signal{}_area'.format(signal))
+    signame = signal_name(signal)
+    if not os.path.exists(twoD.tag + '/' + '{}_area/card.txt'.format(signame)):
+        print('{}/{}_area/card.txt does not exist, making card'.format(twoD.tag, signame))
+        subset = twoD.ledger.select(_select_signal, signame, tf)
+        twoD.MakeCard(subset, '{}_area'.format(signame))
 
     # Now run Combine's Goodness of Fit method, either on Combine or locally. 
     if condor == False:
         twoD.SignalInjection(
-            'signal{}_area'.format(signal), ntoys=nToys, injectAmount=injectedAmount,
+            '{}_area'.format(signame), ntoys=nToys, injectAmount=injectedAmount,
             condor=False
         )
-	# Once finished, we can plot the results immediately from the output rootfile.
+        # Once finished, we can plot the results immediately from the output rootfile.
     else:
-	# 500 (default) toys, split across 50 condor jobs
+        # 500 (default) toys, split across 50 condor jobs
         twoD.SignalInjection(
-            'signal{}_area'.format(signal), ntoys=nToys, injectAmount=injectedAmount,
+            '{}_area'.format(signame), ntoys=nToys, injectAmount=injectedAmount,
             condor=True, njobs=50
         )
-	# If submitting GoF jobs on condor, you must first wait for them to finish before plotting. 
-	print('Jobs successfully submitted - you can run plot_GoF after the jobs have finished running to plot results')
+        # If submitting GoF jobs on condor, you must first wait for them to finish before plotting. 
+        print('Jobs successfully submitted - you can run plot_GoF after the jobs have finished running to plot results')
     
     
 def plot_GoF(signal, tf='', condor=False):
@@ -453,7 +489,7 @@ def plot_GoF(signal, tf='', condor=False):
     Plot the Goodness of Fit as the measured saturated test statistic in data 
     compared against the distribution obtained from the toys. 
     '''
-    plot.plot_gof(savedirname, 'signal{}_area'.format(signal), condor=condor)
+    plot.plot_gof(savedirname, '{}_area'.format(signal_name(signal)), condor=condor)
 
     
 def plot_signalinjection(signal, tf='', injectedAmount=2000.000, nToys=500, condor=False):
@@ -462,28 +498,26 @@ def plot_signalinjection(signal, tf='', injectedAmount=2000.000, nToys=500, cond
     compared against the distribution obtained from the toys. 
     '''
 
-#     plot.plot_signalInjection(savedirname, 'signal{}_area'.format(signal), condor=condor)
-    plot.plot_signalInjection(savedirname, 'signal{}_area'.format(signal), injectedAmount=injectedAmount, seed=123456, stats=True, condor=False)
+#     plot.plot_signalInjection(savedirname, '{}_area'.format(signal_name(signal)), condor=condor)
+    plot.plot_signalInjection(savedirname, '{}_area'.format(signal_name(signal)), injectedAmount=injectedAmount, seed=123456, stats=True, condor=False)
     
     
     
 if __name__ == "__main__":
    
-   make_workspace()
+    make_workspace()
    
 
-   if args.signal:
-	print("Processing single signal: {}...".format(args.signal))
+    if args.signal:
+        print("Processing single signal: {}...".format(args.signal))
         process_signals([args.signal],study)
 
-   elif args.senario_fit == 'RSGluon':
+    elif args.senario_fit == 'RSGluon':
         print("Processing RSGluon signals...")
         RSGluon_signals = load_signals_from_json('jsons/signals.json', args.senario_fit)
         process_signals(RSGluon_signals, study)
-   elif args.senario_fit == 'ZPrime':
+    elif args.senario_fit == 'ZPrime':
         print("Processing ZPrime signals...")
         ZPrime_signals = load_signals_from_json('jsons/signals.json', args.senario_fit)
         process_signals(ZPrime_signals, study)
  
-
-
